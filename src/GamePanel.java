db37894963400987e299519caf9c460d88dd05b7
Gergo Muller <bejla94@gmail.com>
2016-05-09 14:31:38 +0200

    GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -23,9 +23,21 @@ public class GamePanel extends JPanel
 			for(int j=0;j<20;j++)
 			{
 				Image[] images = view.control.getDrawData(i,j);
-				g.drawImage(images[0], i*40, j*40, this); 
+				g.drawImage(images[0], i*50, j*50, 50,50,this); 
+				if(images.length == 2 )
+				{
+					g.drawImage(images[1], i*50, j*50,50,50,this);
+				}
+					
+					
 			}
 		}
+		CharGraphics OGrafInfo = view.control.ODraw();
+		CharGraphics JGrafInfo = view.control.JDraw();
+		CharGraphics RGrafInfo = view.control.RDraw();
+		g.drawImage(OGrafInfo.getImage(), OGrafInfo.getloc().getX()*50,OGrafInfo.getloc().getY()*50, 50 , 50,this);
+		g.drawImage(JGrafInfo.getImage(), JGrafInfo.getloc().getX()*50,JGrafInfo.getloc().getY()*50, 50 , 50,this);
+		g.drawImage(RGrafInfo.getImage(), RGrafInfo.getloc().getX()*50,RGrafInfo.getloc().getY()*50, 50 , 50,this);
         
 	}
 	
